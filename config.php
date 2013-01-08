a00b4c3714fd132641a002a1a7d8b2550a52f112
David M. Lemcoe Jr <david@lemcoe.com>
2013-01-08 14:09:29 -0500

    Added config file and updated the header on the PHPPwinty.php file.

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Please enter your Merchant ID and API KEY for your Pwinty.com account.
+ *
+ * Get yours at https://dashboard.pwinty.com/Account/Register
+ */ 
+
+define('PWINTY_MERCHANTID', 'Put your Merchant ID here');
+define('PWINTY_APIKEY', 'Put your API key here');
+
+# Set to TRUE when you are ready to make real requests.
+define('PWINTY_LIVE', FALSE);
+
+?>
